a1828c6c3069b7b44124b034aa26048621ae454d
kar-21 <karthiks210198@gmail.com>
2020-11-10 09:31:04 +0530

    airline & passanger routes

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose =  require('mongoose');
-const cors = require('cors');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const env = require("./environments/env.envirornment");
-const indexRouter = require('./routes/login');
+const indexRouter = require("./routes/login");
+const airlineRouter = require("./routes/airline");
 
 const app = express();
 const port = 3000;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/login', indexRouter);
+app.use("/login", indexRouter);
+app.use("/", airlineRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -34,11 +36,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 app.listen(port);
