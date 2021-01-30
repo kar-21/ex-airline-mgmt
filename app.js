94c2d02188d9886c50382d06548ded28aaf1df06
kar-21 <karthiks210198@gmail.com>
2021-01-30 19:24:56 +0530

    checking for dynamic port set in env

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
-app.listen(process.env.NODE_ENV === "development" ? port : 8080);
+app.listen(process.env.PORT || port);
 
 mongoose.connect(process.env.MONGODB_API, {
   useNewUrlParser: true,
